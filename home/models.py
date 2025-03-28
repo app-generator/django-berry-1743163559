e2560f4a584d2994bde1be794f8a9ef3ad366562
App Generator <51070104+app-generator@users.noreply.github.com>
2025-03-28 14:06:08 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    username = models.CharField(max_length=255, null=True, blank=True)
+    mobile = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Category(models.Model):
+
+    #__Category_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Category_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Category")
+        verbose_name_plural = _("Category")
+
+
+class Event(models.Model):
+
+    #__Event_FIELDS__
+    title = models.CharField(max_length=255, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    #__Event_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Event")
+        verbose_name_plural = _("Event")
+
+
+
+#__MODELS__END
